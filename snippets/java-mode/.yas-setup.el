11cfc36b7140c66fe78f1da1883ac8aad4552104
Larry Daffner <vizzie@flamingpackets.net>
2026-01-18 04:38:34 -0600

    New improved lrd/pkg-from-dir

--- snippets/java-mode/.yas-setup.el
+++ snippets/java-mode/.yas-setup.el
@@ -1,17 +1,12 @@
 ;; functions to guess package name based on path
 ;; assumes that the treeis rooted at src or java
 ;; Used by YASnippet snippets to automatically guess package name
-(defun lrd/pkg-src-path (dir &optional desc)
-  "Return components of directory path up to src or java, or nil if not found"
-  (let ((parent (file-name-directory (directory-file-name dir)))
-        (child (file-name-nondirectory (directory-file-name dir))))
-    (cond ((equal child "") nil)
-           ((or (equal child "src") (equal child "java")) desc)
-           (t (lrd/pkg-src-path parent (cons child desc))))))
-
 (defun lrd/pkg-from-path (dir)
   "Return a java package name from the source file path, or nil if none found"
-  (message dir)
-  (let ((ret (lrd/pkg-src-path dir)))
-    (cond (ret (mapconcat 'identity ret "."))
-          (t ""))))
+  (let* ((projroot (and (project-current)
+                        (project-root (project-current))))
+         (srcdir (locate-dominating-file default-directory "src"))
+         (toplevel (or projroot (and srcdir (expand-file-name srcdir) default-directory)))
+         (relative-path (file-relative-name default-directory toplevel)))
+    (dolist (prefix '("/" "src/" "main/" "java/")) (setq relative-path (string-remove-prefix prefix relative-path)))
+    (string-replace "/" "." (directory-file-name relative-path))))
